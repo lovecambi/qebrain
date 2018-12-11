d3f6da01052f64b22438e5c22a6b74344a7b760f
Fan, Kai <k.fan@alibaba-inc.com>
2018-12-11 02:01:37 -0800

    bug fix in avg_checkpoints

--- qe_model.py
+++ qe_model.py
@@ -846,7 +846,7 @@ def load_expert_weights(exp_model_dir, model, session):
 
     var_values = {}
     for name, shape in var_list:
-        if name != "global_step" and not "optim" in name and not "words_per_sec" in name:
+        if name != "global_step" and not "optim" in name and not "skip_count" in name:
             if 'inputter_0/w_embs' in name:
                 var_values["embeddings/encoder/embedding_encoder:0"] = reader.get_tensor(name)
             elif 'inputter_1/w_embs' in name:
@@ -926,7 +926,7 @@ def avg_checkpoints(model_dir, num_last_checkpoints, global_step,
         var_values[name] /= len(checkpoints)
 
     with tf.Graph().as_default():
-        tf_vars = [tf.get_variable(v, shape=var_values[v].shape, dtype=var_dtypes[name]) for v in var_values]
+        tf_vars = [tf.get_variable(name, shape=var_values[name].shape, dtype=var_dtypes[name]) for v in var_values]
 
         placeholders = [tf.placeholder(v.dtype, shape=v.shape) for v in tf_vars]
         assign_ops = [tf.assign(v, p) for (v, p) in zip(tf_vars, placeholders)]
